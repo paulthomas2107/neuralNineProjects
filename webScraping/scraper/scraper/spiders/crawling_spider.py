561f0f56a1eb7dfbbc7ae03c4a8d40528348c252
Paul Thomas <paulthomas2107@gmail.com>
2022-11-25 09:50:48 +0000

    Scrapy.....

--- webScraping/scraper/scraper/spiders/crawling_spider.py
+++ webScraping/scraper/scraper/spiders/crawling_spider.py
@@ -9,4 +9,12 @@ class CrawlingSpider(CrawlSpider):
 
     rules = (
         Rule(LinkExtractor(allow="catalogue/category")),
+        Rule(LinkExtractor(allow="catalogue", deny="category"), callback="parse_item"),
     )
+
+    def parse_item(self, response):
+        yield {
+            "title": response.css(".product_main h1::text").get(),
+            "price": response.css(".price_color::text").get(),
+            "availability": response.css(".availability::text")[2].get(),
+        }
