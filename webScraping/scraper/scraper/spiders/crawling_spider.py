34c842c6d99b01b61ce6d09d911a1f4b22940a49
Paul Thomas <paulthomas2107@gmail.com>
2022-11-25 10:05:48 +0000

    Scrapy.....

--- webScraping/scraper/scraper/spiders/crawling_spider.py
+++ webScraping/scraper/scraper/spiders/crawling_spider.py
@@ -1,20 +1,25 @@
+from abc import ABC
+
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
 
-class CrawlingSpider(CrawlSpider):
+class CrawlingSpider(CrawlSpider, ABC):
     name = "myCrawler"
     allowed_domains = ["toscrape.com"]
     start_urls = ["http://books.toscrape.com/"]
+
+    PROXY_SERVER = ""
 
     rules = (
         Rule(LinkExtractor(allow="catalogue/category")),
         Rule(LinkExtractor(allow="catalogue", deny="category"), callback="parse_item"),
     )
 
-    def parse_item(self, response):
+    @staticmethod
+    def parse_item(response):
         yield {
             "title": response.css(".product_main h1::text").get(),
             "price": response.css(".price_color::text").get(),
-            "availability": response.css(".availability::text")[2].get(),
+            "availability": response.css(".availability::text")[1].get().replace("\n", "").replace(" ", ""),
         }
