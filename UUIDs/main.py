44f72d7cf02d373487968337f9678e20418da6e1
Paul Thomas <paulthomas2107@gmail.com>
2022-12-05 13:51:14 +0000

    More...

--- UUIDs/main.py
+++ UUIDs/main.py
@@ -2,15 +2,20 @@ import pandas as pd
 import uuid
 
 data = pd.DataFrame({
-    "id":  [1, 2, 3, 4, 5, 6],
+    "id":  [uuid.uuid4() for _ in range(6)],
     "value": [40, 20, 10, 20, 30, 30],
     "value2": [1, 1, 1, 0, 0, 1]
 })
 
 data = data.set_index('id')
-
 existing = pd.read_csv("file.csv")
 existing = existing.set_index("id")
 
 combine = pd.concat([existing, data], verify_integrity=True)
 print(combine)
+
+test = uuid.uuid4()
+for _ in range(9000000):
+    if uuid.uuid4() == test:
+        print("Match")
+print("Completed")
