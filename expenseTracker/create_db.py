d374e233794cfa0f4e67b3e5bb929e09877e1067
Paul Thomas <paulthomas2107@gmail.com>
2023-01-16 12:02:04 +0000

    More...

---
+++ expenseTracker/create_db.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+conn = sqlite3.connect("expenses.db")
+
+cur = conn.cursor()
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS expenses
+(id INTEGER PRIMARY KEY,
+date DATE, 
+description TEXT,
+category TEXT,
+price REAL)
+""")
+
+conn.commit()
+conn.close()
