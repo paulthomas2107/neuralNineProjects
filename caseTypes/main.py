8f799858f6cb5db58d3f48566ca703f433841b78
Paul Thomas <paulthomas2107@gmail.com>
2023-01-18 14:12:06 +0000

    More...

---
+++ caseTypes/main.py
@@ -0,0 +1,18 @@
+# Camel Case: myNameVar
+
+# Snake Case: my_name_var
+
+# Pascal Case / Capital Camel Case: MyVariableName
+
+# Kebab Case / LISP Case: my-name-variable
+
+# Upper Snake Case: MY-VARIABLE-NAME
+
+# Flat Case: myvarname
+
+
+
+
+
+
+
