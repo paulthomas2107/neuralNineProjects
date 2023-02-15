79b97cba6fc45f8917b87e94da382be9f9895635
Paul Thomas <paulthomas2107@gmail.com>
2023-02-15 15:13:18 +0000

    More...

--- financialDashboard/main.py
+++ financialDashboard/main.py
@@ -17,18 +17,41 @@ def load_data(ticker1, ticker2, start, end):
 
 
 def plot_data(data, indicators, sync_axis=None):
-    pass
+    df = data
+    gain = df.Close > df.Open
+    loss = df.Open > df.Close
+    width = 12 * 60 * 60 * 1000
+
+    if sync_axis is not None:
+        p = figure(x_axis_type="datetime", tools="pan,wheel_zoom,box_zoom,reset,save", width=1000,
+                   x_range=sync_axis)
+    else:
+        p = figure(x_axis_type="datetime", tools="pan,wheel_zoom,box_zoom,reset,save", width=1000)
+
+    p.xaxis.major_label_orientation = math.pi / 4
+    p.grid.grid_line_alpha = 0.25
+
+    p.segment(df.index, df.High, df.index, df.Low, color="black")
+    p.vbar(df.index[gain], width, df.Open[gain], df.Close[gain], fill_color="#00ff00", line_color="#00ff00")
+    p.vbar(df.index[loss], width, df.Open[loss], df.Close[loss], fill_color="#ff0000", line_color="#ff0000")
+
+    return p
 
 
 def on_button_click(ticker1, ticker2, start, end, indicators):
-    pass
+    df1, df2 = load_data(ticker1, ticker2, start, end)
+    p1 = plot_data(df1, indicators)
+    p2 = plot_data(df2, indicators, sync_axis=p1.x_range)
+    curdoc().clear()
+    curdoc().add_root(layout)
+    curdoc().add_root(row(p1, p2))
 
 
 stock1_text = TextInput(title="Stock 1")
 stock2_text = TextInput(title="Stock 2")
-date_picker_from = DatePicker(tite="Start Date", value="2020-01-01", min_date="2000-01-01",
+date_picker_from = DatePicker(title="Start Date", value="2020-01-01", min_date="2000-01-01",
                               max_date=dt.datetime.now().strftime("%Y-%m-%d"))
-date_picker_to = DatePicker(tite="End Date", value="2020-02-01", min_date="2000-01-01",
+date_picker_to = DatePicker(title="End Date", value="2020-02-01", min_date="2000-01-01",
                             max_date=dt.datetime.now().strftime("%Y-%m-%d"))
 indicator_choice = MultiChoice(options=["100 Day SMA", "30 Day SMA", "Linear Regression Line"])
 
@@ -41,4 +64,3 @@ layout = column(stock1_text, stock2_text, date_picker_from, date_picker_to, indi
 
 curdoc().clear()
 curdoc().add_root(layout)
-
