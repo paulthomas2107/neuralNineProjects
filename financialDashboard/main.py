d0cf1fc1d9cc2d0653e1516b6b455a71a3463427
Paul Thomas <paulthomas2107@gmail.com>
2023-02-15 14:51:29 +0000

    More...

--- financialDashboard/main.py
+++ financialDashboard/main.py
@@ -28,4 +28,17 @@ stock1_text = TextInput(title="Stock 1")
 stock2_text = TextInput(title="Stock 2")
 date_picker_from = DatePicker(tite="Start Date", value="2020-01-01", min_date="2000-01-01",
                               max_date=dt.datetime.now().strftime("%Y-%m-%d"))
+date_picker_to = DatePicker(tite="End Date", value="2020-02-01", min_date="2000-01-01",
+                            max_date=dt.datetime.now().strftime("%Y-%m-%d"))
+indicator_choice = MultiChoice(options=["100 Day SMA", "30 Day SMA", "Linear Regression Line"])
+
+load_button = Button(label="Load Data", button_type="success")
+load_button.on_click(lambda: on_button_click(stock1_text.value, stock2_text.value,
+                                             date_picker_from.value, date_picker_to.value,
+                                             indicator_choice.value))
+
+layout = column(stock1_text, stock2_text, date_picker_from, date_picker_to, indicator_choice, load_button)
+
+curdoc().clear()
+curdoc().add_root(layout)
 
