d6292d48ca6f1faecbd4e70346979ee761f6b748
Paul Thomas <paulthomas2107@gmail.com>
2023-02-20 11:43:20 +0000

    More...

--- pyneconeFramework/pyneconeFramework/pyneconeFramework.py
+++ pyneconeFramework/pyneconeFramework/pyneconeFramework.py
@@ -1,42 +1,24 @@
-# type: ignore
-"""Welcome to Pynecone! This file outlines the steps to create a basic app."""
-from pcconfig import config
-
 import pynecone as pc
-
-docs_url = "https://pynecone.io/docs/getting-started/introduction"
-filename = f"{config.app_name}/{config.app_name}.py"
 
 
 class State(pc.State):
-    """The app state."""
+    count = 0
 
-    pass
+    def increment(self):
+        self.count += 1
+
+    def decrement(self):
+        self.count -= 1
 
 
-def index() -> pc.Component:
-    return pc.center(
-        pc.vstack(
-            pc.heading("Welcome to Pynecone!", font_size="2em"),
-            pc.box("Get started by editing ", pc.code(filename, font_size="1em")),
-            pc.link(
-                "Check out our docs!",
-                href=docs_url,
-                border="0.1em solid",
-                padding="0.5em",
-                border_radius="0.5em",
-                _hover={
-                    "color": "rgb(107,99,246)",
-                },
-            ),
-            spacing="1.5em",
-            font_size="2em",
-        ),
-        padding_top="10%",
+def index():
+    return pc.hstack(
+        pc.button("-", color_scheme="red", border_radius="1em", on_click=State.decrement),
+        pc.heading(State.count, font_size="2em"),
+        pc.button("+", color_scheme="green", border_radius="1em", on_click=State.increment),
     )
 
 
-# Add state and page to the app.
 app = pc.App(state=State)
-app.add_page(index)
+app.add_page(index, path="/")
 app.compile()
