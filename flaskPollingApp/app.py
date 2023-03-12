05d6814a7f4ea72783eb53d0e7972fff6894e05c
Paul Thomas <paulthomas2107@gmail.com>
2023-03-12 09:47:05 +0000

    More

--- flaskPollingApp/app.py
+++ flaskPollingApp/app.py
@@ -5,11 +5,47 @@ from flask import Flask, render_template, redirect, request, url_for, make_respo
 
 app = Flask(__name__, template_folder="templates")
 
+if not os.path.exists("polls.csv"):
+    structure = {
+        "id": [],
+        "poll": [],
+        "option1": [],
+        "option2": [],
+        "option3": [],
+        "votes1":  [],
+        "votes2": [],
+        "votes3": []
+    }
+
+    pd.DataFrame(structure).set_index("id").to_csv("polls.csv")
+
+polls_df = pd.read_csv("polls.csv").set_index("id")
+
 
 @app.route("/")
 def index():
-    return "Hello, Paul"
+    return render_template("index.html", polls=polls_df)
+
+
+@app.route("/polls/<id>")
+def polls(id):
+    poll = polls_df.loc[int(id)]
+    return str(poll)
+
+
+@app.route("/polls", methods=["GET", "POST"])
+def create_poll():
+    if request.method == "GET":
+        pass
+    elif request.method == "POST":
+        pass
+
+
+@app.route("/vote/<id>/<option>")
+def vote(id, option):
+    pass
 
 
 if __name__ == "__main__":
     app.run(host="localhost", debug=True)
+
