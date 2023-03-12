8edb97e432a10c11ac530f439437c6c76a980ee6
Paul Thomas <paulthomas2107@gmail.com>
2023-03-12 10:45:51 +0000

    Complete

--- flaskPollingApp/app.py
+++ flaskPollingApp/app.py
@@ -49,9 +49,14 @@ def create_poll():
 
 @app.route("/vote/<id>/<option>")
 def vote(id, option):
-    polls_df.at[int(id), "votes"+str(option)] += 1
-    polls_df.to_csv("polls.csv")
-    return redirect(url_for("polls", id=id))
+    if request.cookies.get(f"vote_{id}_cookie") is None:
+        polls_df.at[int(id), "votes"+str(option)] += 1
+        polls_df.to_csv("polls.csv")
+        response = make_response(redirect(url_for("polls", id=id)))
+        response.set_cookie(f"vote_{id}_cookie", str(option))
+        return response
+    else:
+        return "Cannot vote more than once !"
 
 
 if __name__ == "__main__":
