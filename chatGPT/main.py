1e586e2cff5bdf40074394d7cdcbcb9ae1cf9268
Paul Thomas <paulthomas2107@gmail.com>
2023-03-20 09:23:26 +0000

    More....

---
+++ chatGPT/main.py
@@ -0,0 +1,16 @@
+import openai
+
+API_KEY = open("API_KEY", "r").read()
+openai.api_key = API_KEY
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+            "role": "user",
+            "content": "What is the difference between celsius and fahrenheit ?"
+        }
+    ]
+)
+
+print(response)
