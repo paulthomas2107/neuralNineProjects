13776cc3a1f0cbdb30796b9fd4b4cae6180a4375
Paul Thomas <paulthomas2107@gmail.com>
2023-04-14 16:49:40 +0100

    More....

---
+++ interfaceStubs/circle_module/circle.pyi
@@ -0,0 +1,24 @@
+from typing import Tuple
+def area(radius: float) -> float:
+    """
+    Calculate area from a radius
+    :param radius
+    :return: area: float
+    """
+    ...
+
+
+def circumference(radius: float) -> float:
+    ...
+
+
+def radius_from_area(area: float) -> float:
+    ...
+
+
+def radius_from_circumference(circumference: float) -> float:
+    ...
+
+
+def circle_info(radius: float) -> float:
+   ...
