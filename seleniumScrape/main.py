f5e0d873ae198bcda5e3257db6c1c859624fd871
Paul Thomas <paulthomas2107@gmail.com>
2023-10-27 22:11:04 +0100

    More....

--- seleniumScrape/main.py
+++ seleniumScrape/main.py
@@ -2,11 +2,16 @@ import time
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
 url = 'http://www.neuralnine.com/books'
 
