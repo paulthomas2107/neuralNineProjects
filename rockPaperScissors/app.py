5a03b46e8d881406e9463eb630cfcac2262ce247
Paul Thomas <paulthomas2107@gmail.com>
2024-01-28 10:18:50 +0000

    More....

--- rockPaperScissors/app.py
+++ rockPaperScissors/app.py
@@ -4,6 +4,7 @@ done = False
 wins, losses, ties = 0, 0, 0
 
 names = {'R': 'Rock', 'P': 'Paper', 'S': 'Scissors'}
+loses = {'R': 'P', 'P': 'S', 'S': 'R'}
 
 while not done:
     choice = input("Please provide your next move (R, P, S or Q) : ")
@@ -11,26 +12,12 @@ while not done:
     if choice == cpu_choice:
         print(f"Tie. You both chose {names[choice]}")
         ties += 1
-    elif choice == 'R':
-        if cpu_choice == 'P':
-            print(f'CPU wins! You chose {names[choice]}, the CPU chose {names[cpu_choice]}')
+    elif choice in ['R', 'P', 'S']:
+        if cpu_choice == loses[choice]:
+            print(f'CPU Wins. You chose {names[choice]}, the CPU chose {names[cpu_choice]}')
             losses += 1
         else:
-            print(f'You win! You chose {names[choice]}, the CPU chose {names[cpu_choice]}')
-            wins += 1
-    elif choice == 'P':
-        if cpu_choice == 'S':
-            print(f'CPU wins! You chose {names[choice]}, the CPU chose {names[cpu_choice]}')
-            losses += 1
-        else:
-            print(f'You win! You chose {names[choice]}, the CPU chose {names[cpu_choice]}')
-            wins += 1
-    elif choice == 'S':
-        if cpu_choice == 'R':
-            print(f'CPU wins! You chose {names[choice]}, the CPU chose {names[cpu_choice]}')
-            losses += 1
-        else:
-            print(f'You win! You chose {names[choice]}, the CPU chose {names[cpu_choice]}')
+            print(f'You Win. You chose {names[choice]}, the CPU chose {names[cpu_choice]}')
             wins += 1
     elif choice == 'Q':
         done = True
