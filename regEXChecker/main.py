91935cbb1d6158f34046862b4f52bcf9cfb51a19
Paul Thomas <paulthomas2107@gmail.com>
2024-02-09 09:35:26 +0000

    More....

---
+++ regEXChecker/main.py
@@ -0,0 +1,19 @@
+import re
+import tkinter as tk
+
+
+def find_matches():
+    pass
+
+
+def check_match():
+    pass
+
+
+root = tk.Tk()
+root.title('RegEx Checker')
+
+regex_label = tk.Label(root, text='Enter Regex:')
+regex_label.grid(row=0, column=0, padx=5, pady=5)
+
+root.mainloop()
