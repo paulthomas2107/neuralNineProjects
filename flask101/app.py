fda34c65abac1041b7cb21ef6f0dae7a467b4a9d
Paul Thomas <paulthomas2107@gmail.com>
2024-02-25 14:36:00 +0000

    More....

--- flask101/app.py
+++ flask101/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,31 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "<h1>Hello, Paul</h1>"
+
+
+@app.route('/hello')
+def hello():
+    return "Hello World"
+
+
+@app.route('/greet/<name>')
+def greet(name):
+    return f"Hello {name}"
+
+
+@app.route('/add/<int:number1>/<int:number2>')
+def add(number1, number2):
+    return f"{number1} + {number2} = {number1 + number2}"
+
+
+@app.route('/handle_url_params')
+def handle_params():
+    if 'greeting' in request.args.keys() and 'name' in request.args.keys():
+        greeting = request.args.get('greeting')
+        name = request.args.get('name')
+        return f'{greeting} {name}'
+    else:
+        return 'Parameters not available'
 
 
 if __name__ == '__main__':
